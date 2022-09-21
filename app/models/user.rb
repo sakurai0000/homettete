79df1f06d4f0908fce2eeb63b7e1e7a8ecafe3ed
sakurai0000 <107847812+sakurai0000@users.noreply.github.com>
2022-09-21 04:15:35 +0000

    アイコン修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,11 +32,11 @@ class User < ApplicationRecord
 # プロフ画像用
   has_one_attached :profile_image
 def get_profile_image(width, height)
-  unless profile_image.attached?
-    file_path = Rails.root.join('app/assets/images/no_image.jpg')
-    profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
-  end
-  profile_image.variant(resize_to_limit: [width, height]).processed
+ unless profile_image.attached?
+  file_path = Rails.root.join('app/assets/images/no_image.png')
+  profile_image.attach(io: File.open(file_path), filename: 'default-image.png', content_type: 'image/png')
+ end
+  profile_image.variant(gravity: :center, resize: "#{width}x#{height}^", crop: "#{width}x#{height}+0+0").processed
 end
 
  # サーチ機能
